10a6031a0b5aaad60bae65a228063c527273f937
carrkarr <kcarrl@yahoo.com.mx>
2022-07-14 21:46:12 -0500

    prod_gitnoremod

--- mginvoice/mginvoice/settings.py
+++ mginvoice/mginvoice/settings.py
@@ -166,6 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+''' 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     # BASE_DIR.child('static'),
@@ -173,6 +174,15 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
+'''
+
+if DEBUG:
+    STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
